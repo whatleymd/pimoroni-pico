91b0b74e1fca5034d9fade8f1f5ce01969b2b40a
whatleymd <whatleymd1994@gmail.com>
2024-06-12 17:42:03 -0400

    Updated cmake

--- micropython/modules/animatedgif/micropython.cmake
+++ micropython/modules/animatedgif/micropython.cmake
@@ -1,2 +1,25 @@
-add_library(gifdec STATIC gifdec.c gifdec.cpp AnimatedGIF.cpp)
+# The following lines are typical in a CMakeLists.txt or similar CMake build configuration for MicroPython integration
+
+set(GIFDEC_SRC
+    gifdec.c
+    gifdec.cpp
+    AnimatedGIF.cpp
+)
+
+set(GIFDEC_HEADERS
+    gifdec.h
+    AnimatedGIF.h
+    gif.inl
+)
+
+# Specify the target for the GIF decoder
+add_library(gifdec STATIC ${GIFDEC_SRC} ${GIFDEC_HEADERS})
+
+# Link necessary libraries
 target_link_libraries(gifdec pico_stdlib hardware_pio hardware_dma hardware_interp)
+
+# Include directories for the GIF decoder
+target_include_directories(gifdec PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+
+# Define the module in MicroPython
+set(MICROPYTHON_EXTRA_C_MODULES ${MICROPYTHON_EXTRA_C_MODULES} ${CMAKE_CURRENT_SOURCE_DIR}/gifdec)
