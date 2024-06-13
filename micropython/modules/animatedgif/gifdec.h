f194e7c8127a59204739609728d798e39f3c88df
whatleymd <whatleymd1994@gmail.com>
2024-06-12 20:30:33 -0400

    Hecking current_flags

--- micropython/modules/animatedgif/gifdec.h
+++ micropython/modules/animatedgif/gifdec.h
@@ -3,6 +3,8 @@
 
 extern const mp_obj_type_t GIF_type;
 
+extern uint8_t current_flags;
+
 extern mp_obj_t _GIF_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args);
 extern mp_obj_t _GIF_del(mp_obj_t self_in);
 extern mp_obj_t _GIF_openRAM(mp_obj_t self_in, mp_obj_t buffer);
