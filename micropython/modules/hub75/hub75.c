c6341f064f9f7d5f93d08fbb54ed6b90c1582ab2
whatleymd <whatleymd1994@gmail.com>
2024-06-14 00:51:33 -0400

    moar

--- micropython/modules/hub75/hub75.c
+++ micropython/modules/hub75/hub75.c
@@ -11,7 +11,7 @@ MP_DEFINE_CONST_FUN_OBJ_2(Hub75_update_obj, Hub75_update);
 MP_DEFINE_CONST_FUN_OBJ_2(Hub75_set_brightness_obj, Hub75_set_brightness);
 
 mp_obj_t Hub75_set_brightness(mp_obj_t self_in, mp_obj_t brightness) {
-    _Hub75_obj_t *self = MP_OBJ_TO_PTR2(self_in, _Hub75_obj_t);
+    _Hub75_obj_t *self = MP_OBJ_TO_PTR(self_in);  // Use MP_OBJ_TO_PTR
     float brightness_value = mp_obj_get_float(brightness);
     self->hub75->set_brightness(brightness_value);
     return mp_const_none;
--- micropython/modules/hub75/hub75.h
+++ micropython/modules/hub75/hub75.h
@@ -15,3 +15,10 @@ extern mp_obj_t Hub75_set_pixel(size_t n_args, const mp_obj_t *pos_args, mp_map_
 extern mp_obj_t Hub75_clear(mp_obj_t self_in);
 extern mp_obj_t Hub75_update(mp_obj_t self_in, mp_obj_t graphics_in);
 extern mp_obj_t Hub75_set_brightness(mp_obj_t self_in, mp_obj_t brightness_in);  // Add this line
+
+// Definition of _Hub75_obj_t
+typedef struct _Hub75_obj_t {
+    mp_obj_base_t base;
+    Hub75* hub75;
+    void *buf;
+} _Hub75_obj_t;
